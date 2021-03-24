48960580609460ca4371a854819886effd625422
raulc <raulc@Megatron-2>
2021-03-24 22:07:36 +0100

    Cancellato package prova

--- Skribbl/src/it/unibs/pajc/prova/Prova.java
+++
@@ -1,6 +0,0 @@
-package it.unibs.pajc.prova;
-
-public class Prova {
-	public final static String MATCH_STARTED_CLIENT = "matchstarted";
-	public final static String MATCH_STARTED_SERVER = "!matchstarted";
-}
